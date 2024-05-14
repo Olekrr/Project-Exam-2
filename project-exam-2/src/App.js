b27121734a9d528aac6f63222e5c388b47edd7af
Olekrr <93515165+Olekrr@users.noreply.github.com>
2024-05-14 02:58:01 +0200

    shell layout

--- project-exam-2/src/App.js
+++ project-exam-2/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <h1>Welcome to Holidaze</h1>
+              <p>Your one-stop destination for holiday bookings!</p>
+            </>
+          } />
+        </Routes>
+      </Layout>
+    </Router>
   );
-}
+};
 
 export default App;
