db4bc29a633f63f5d378f4528c7830c0fc513947
Olekrr <93515165+Olekrr@users.noreply.github.com>
2024-05-26 18:52:05 +0200

    changed names back

--- src/components/auth/login/Login.jsx
+++ src/components/auth/login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import TextInput from "../../utils/textinputintermediate/TextInput.intermediate";
+import TextInput from "../../utils/textinput/TextInput";
 import { useAuth } from "../../../contexts/authContext";
 import "./login.scss";
 
